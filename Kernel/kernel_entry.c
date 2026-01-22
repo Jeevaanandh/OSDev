f7b7ad83000d6a5012dea5f3b7e57ed40cb2a0eb
jeevaanandh <anandhjeeva215@gmail.com>
2026-01-22 18:52:00 +0530

    The printk function works using a co-ordinate system. Also wrote a clear screen function. (STABLE COMMIT)

--- Kernel/kernel_entry.c
+++ Kernel/kernel_entry.c
@@ -1,5 +1,7 @@
-
 #define VGA 0xB8000
+
+int cursor_x=0;
+int cursor_y=0;
 
 
 void printk(const char *msg) {
@@ -7,13 +9,42 @@ void printk(const char *msg) {
     
     int i = 0;
     while (msg[i]) {
-        vga[i] = (0x0F << 8) | msg[i];
+        int address= cursor_x+ (cursor_y*80);
+        if(msg[i]=='\n'){
+            cursor_y+=1;
+            cursor_x=0;
+            i+=1;
+            continue;
+        }
+
+        
+        unsigned short text = (0x0F << 8) | msg[i];
+        
+
+        vga[address]= text;
+        cursor_x+=1;
         i++;
     }
+  
+}
+
+void clear_screen(){
+    volatile unsigned short *vga= (unsigned short*)VGA;
+
+    unsigned short blank= (0x0F)<<8 | ' ';
+
+    for(int i=0;i <80*25; i++){
+        vga[i]=blank;
+    }
+
+    cursor_x=0;
+    cursor_y=0;
 }
 
 void kernel_entry() {
-    printk("Hello from C kernel!\n Hello again!!!");
+    clear_screen();
+    printk("Hello from C kernel!\nHello again!!!\n");
+    printk("Second Hello");
     
     while (1);
 }
